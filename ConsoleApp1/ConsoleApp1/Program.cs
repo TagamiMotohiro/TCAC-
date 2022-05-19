63183e6f5f15667c572ce679b5f1e2f4e987e4c4
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2022-05-19 13:26:23 +0900

    a

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string text1 = "TEXT";
+string text2 = "TEXT2";
+Console.WriteLine(text1+text2);
